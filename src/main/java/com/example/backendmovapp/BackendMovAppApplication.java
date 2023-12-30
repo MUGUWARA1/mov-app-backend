66f47f23f3623472e200d457d58d0b096477d01e
DEROUICH KARIM <106762744+MUGUWARA1@users.noreply.github.com>
2023-12-30 19:13:25 +0100

    setting the tables and entitites

---
+++ src/main/java/com/example/backendmovapp/BackendMovAppApplication.java
@@ -0,0 +1,31 @@
+package com.example.backendmovapp;
+
+import com.example.backendmovapp.Modele.Commentaire;
+import com.example.backendmovapp.Repository.CommentaireRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BackendMovAppApplication {
+
+    @Autowired
+    private CommentaireRepository commRepo;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendMovAppApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner useSerice(){
+        return args -> {
+
+
+        };
+    }
+
+
+
+}
---
+++ src/test/java/com/example/backendmovapp/BackendMovAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendmovapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMovAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
